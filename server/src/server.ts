892de960084d2343f76887fa11f7054e4057ec86
Mihai Butnariu <mihai.alex.butnariu@gmail.com>
2026-02-19 02:17:51 +0100

    1.40.17

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,35 +1,32 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 // const cors = require("cors");
 // For default imports from packages that export in ES Modules
 // const express = require("express").default;
 import { connectToDatabase } from "./database.js";
 import { router } from "./routes.js";
-import * as dotenv from 'dotenv';
-import 'dotenv/config';
+import * as dotenv from "dotenv";
+import "dotenv/config";
 dotenv.config();
 
-
 // Defining the app and port of the express application
-
-
 
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:4200", // Allow requests from this origin
-  })
+    origin: process.env.NODE_ENV === "production" 
+      ? "https://capirelaterra.it" 
+      : "http://localhost:4200",
+  }),
 );
 
-app.use(express.json({ limit: '5mb' }));
+app.use(express.json({ limit: "5mb" }));
 app.use(router);
 
 connectToDatabase()
   .then(() => {
-    app.listen(3000, () => {
-      console.log(
-        `Server running at http://localhost:3000...`
-      );
+    app.listen(3000, "0.0.0.0", () => {
+      console.log("Server running at http://localhost:3000...");
     });
   })
   .catch((error) => console.log(error));
